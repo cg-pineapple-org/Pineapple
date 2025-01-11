34c3c0ffa7e670c7f320884c2ce7a17cf743691c
lvlnguyen00 <80013922+Lvinguyen0000@users.noreply.github.com>
2025-01-11 21:51:16 +0700

    unimport unused library

--- src/main/java/com/codegym/pineapple/dao/ProductDao.java
+++ src/main/java/com/codegym/pineapple/dao/ProductDao.java
@@ -13,7 +13,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ProductDao {
     private static final Logger logger = LoggerFactory.getLogger(ProductDao.class);
